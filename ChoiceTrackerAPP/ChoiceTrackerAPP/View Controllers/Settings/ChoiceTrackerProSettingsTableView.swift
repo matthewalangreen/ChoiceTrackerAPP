02be7e03e7ae2804c257cc903ac1e39e379e1585
Matt Green <Matt.Green@mead354.org>
2018-09-02 20:12:44 -0700

    added accessory views to cells

--- ChoiceTrackerAPP/ChoiceTrackerAPP/View Controllers/Settings/ChoiceTrackerProSettingsTableView.swift
+++ ChoiceTrackerAPP/ChoiceTrackerAPP/View Controllers/Settings/ChoiceTrackerProSettingsTableView.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ChoiceTrackerProSettingsTableView: UITableViewController {
-   
+    @IBOutlet var monthly: UITableViewCell!
+    @IBOutlet var yearly: UITableViewCell!
+    @IBOutlet var exportData: UITableViewCell!
+    @IBOutlet var themes: UITableViewCell!
+    @IBOutlet var labelYourData: UITableViewCell!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         monthly.accessoryType = .disclosureIndicator
+         yearly.accessoryType = .disclosureIndicator
+         exportData.accessoryType = .disclosureIndicator
+         themes.accessoryType = .disclosureIndicator
+         labelYourData.accessoryType = .disclosureIndicator
+        
+        
+        
         // set footer to blank to hide extra rows
         tableView.tableFooterView = UIView()
     }
