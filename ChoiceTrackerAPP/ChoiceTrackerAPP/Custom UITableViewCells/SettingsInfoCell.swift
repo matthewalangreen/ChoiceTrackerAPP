db73f4389772570db9320c6c10f2c3ca907ea950
Matt Green <Matt.Green@mead354.org>
2018-09-02 20:23:13 -0700

    building the "about" section

---
+++ ChoiceTrackerAPP/ChoiceTrackerAPP/Custom UITableViewCells/SettingsInfoCell.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsInfoCell.swift
+//  ChoiceTrackerAPP
+//
+//  Created by Matt Green on 9/2/18.
+//  Copyright © 2018 Matt Green. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class SettingsInfoCell: UITableViewCell {
+    
+    
+}
