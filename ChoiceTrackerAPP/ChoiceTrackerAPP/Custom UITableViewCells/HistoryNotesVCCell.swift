f814f0a360468fb0b7ad45b6032b4b870d93ea98
Matthew Green <matthewalangreen@gmail.com>
2019-06-13 17:37:07 -0700

    updates to cell

---
+++ ChoiceTrackerAPP/ChoiceTrackerAPP/Custom UITableViewCells/HistoryNotesVCCell.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryNotesVCCell.swift
+//  ChoiceTrackerAPP
+//
+//  Created by Matthew Green on 6/13/19.
+//  Copyright © 2019 Matt Green. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class HistoryNotesVCCell: UITableViewCell {
+}
